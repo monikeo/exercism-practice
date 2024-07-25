2b898dc5fdafd42eeb9e98a186e85aa538b34bb5
dark.moni.281000@gmail.com <dark.moni.281000@gmail.com>
2024-07-25 15:43:15 +0700

    finished kindergarten-garden exercise

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -1,3 +1,25 @@
+const CHILDREN: [&str; 12] = [
+    "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
+    "Kincaid", "Larry",
+];
+
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    todo!("based on the {diagram}, determine the plants the {student} is responsible for");
+    let position = CHILDREN.iter().position(|&object| object == student);
+    if let Some(position) = position {
+        let position = position * 2;
+        diagram
+            .split('\n')
+            .flat_map(|line| {
+                line[position..=position + 1].chars().map(|ch| match ch {
+                    'G' => "grass",
+                    'C' => "clover",
+                    'R' => "radishes",
+                    'V' => "violets",
+                    _ => unreachable!(),
+                })
+            })
+            .collect()
+    } else {
+        Vec::new()
+    }
 }
